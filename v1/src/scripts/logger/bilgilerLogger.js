8b7c14a910603042e3ae438e1105584944ea739c
Can Razboyalı <canrazbonyali@gmail.com>
2022-04-19 09:06:40 +0300

    Logger düzenlemesi (düzeye göre ayrı log dosyasına eklemesi)

--- v1/src/scripts/logger/bilgilerLogger.js
+++ v1/src/scripts/logger/bilgilerLogger.js
@@ -1,17 +1,28 @@
 const winston = require("winston");
+const { json, combine, timestamp } = winston.format;
+
+const errorFilter = winston.format((info, opts) => {
+  return info.level === "error" ? info : false;
+});
+
+const infoFilter = winston.format((info, opts) => {
+  return info.level === "info" ? info : false;
+});
 
 const logger = winston.createLogger({
-//   level: "info",
-  format: winston.format.json(),
-  defaultMeta: { service: "bilgiler-service" }, //bu property araştır
+  level: "info",
+  format: combine(timestamp(), json()),
+  // defaultMeta: { service: "bilgiler-service" }, //bu property araştır
   transports: [
     new winston.transports.File({
-      level: "error",
       filename: "v1/src/logs/errors.log",
+      level: "error",
+      format: errorFilter(),
     }),
     new winston.transports.File({
-      level: "info",
       filename: "v1/src/logs/info.log",
+      level: "info",
+      format: infoFilter(),
     }),
     new winston.transports.File({ filename: "v1/src/logs/allLogs.log" }),
   ],
